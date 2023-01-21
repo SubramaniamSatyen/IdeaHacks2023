734bac401b75c0640fc3b5af43d14e1f4c7f17ab
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-01-21 11:37:54 -0800

    Adding Reader Page

--- client/src/Highscores.js
+++ client/src/Highscores.js
@@ -2,13 +2,13 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { db } from "./firebase";
 import { onValue, ref } from "firebase/database";
+import { useNavigate, Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
-
+const device_reserved = "_device_reading"
 
 function Highscores() {
   const [scores, setScores] = useState([{_id: 1, value: 10}]);
-  const [temp, setTemp] = useState([]);
-
 
   useEffect(() => {
     const query = ref(db, "/");
@@ -22,14 +22,17 @@ function Highscores() {
     });
   }, []);
 
-  console.log(temp)
   return (
     <div className="App">
+      <div className="header">
+        <Button variant="contained" component={Link} to="/reader">Muscle Readings</Button>
+      </div>
       <h1>Highscores: </h1>
+
 
       <div className="scores">
         {scores.length > 0 ? scores.map((score_record) => (
-          (score_record.length >= 2 ?
+          (score_record.length >= 2 && score_record[0] != device_reserved ?
           <div className="score" key={score_record[0]}>
             <div className="text">{score_record[0]}: {score_record[1]}</div>
           </div>
--- client/src/Reader.js
+++ client/src/Reader.js
@@ -1,8 +1,52 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { db } from "./firebase";
+import { onValue, ref } from "firebase/database";
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+
+const total_strength_bars = 12
 
 function Reader() {
+  const [strengthReading, setStrength] = useState(<div className="progressBox"/>);
+  const [score, setScore] = useState(1);
+  const [emptyReading, setEmpty] = useState(new Array(total_strength_bars - score).fill('').map((_, index) => <div className="emptyBox"/>));
+
+
+  useEffect(() => {
+    const query = ref(db, "/");
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+
+      if (snapshot.exists()) {
+        setScore(data._device_reading)
+        console.log(data._device_reading) 
+        setStrength(new Array(data._device_reading).fill('').map((_, index) => <div className="progressBox"/>))
+        setEmpty(new Array(total_strength_bars - data._device_reading).fill('').map((_, index) => <div className="emptyBox"/>))
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
+      <div className="header">
+        <Button variant="contained" component={Link} to="/scores">Highscores</Button>
+      </div>
+      <div className="columnWrapper">
+        <div className="column">
+          <h2>Muscle Reading</h2>
+            <div className="fillSpace">
+              {emptyReading}
+              {strengthReading}
+            </div>
+          </div>
+        <div className="column">
+          <h2># of Repetitions</h2>
+        </div>
+        <div className="column">
+          <h2>Completed Sets</h2>
+        </div>
+      </div>
     </div>
   );
 }
